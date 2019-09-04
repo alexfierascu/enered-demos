5bb1383e9de57043b4cad404fd0d944a8517bd64
Alex Fierascu <ioan-alexandru.fierascu@externals.adidas.com>
2019-09-04 14:30:39 +0300

    Proiect Enered Demo
    - change tests from public to private access
    - add asserts to tests

--- DesignPattern-Factory/src/test/java/TesteEntitati/Teste.java
+++ DesignPattern-Factory/src/test/java/TesteEntitati/Teste.java
@@ -4,36 +4,40 @@ package TesteEntitati;
 import Entitati.FiguraGeometrica;
 import Entitati.FiguriGeometrice;
 import Entitati.FiguriGeometriceFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
 public class Teste {
 
   @Test
-  public void testCerc () {
+  private void testCerc () {
 
     FiguraGeometrica cerculMeu = FiguriGeometriceFactory.creazaFigura(FiguriGeometrice.CERC);
     cerculMeu.calculeazaPerimetru();
+    Assertions.assertEquals(77.28317927830892, cerculMeu.calculeazaPerimetru());
     cerculMeu.afiseazaPerimetru();
   }
 
 
   @Test
-  public void testPatrat () {
+  private void testPatrat () {
 
     FiguraGeometrica patratulMeu = FiguriGeometriceFactory.creazaFigura(FiguriGeometrice.PATRAT);
     patratulMeu.calculeazaPerimetru();
+    Assertions.assertEquals(20, patratulMeu.calculeazaPerimetru());
     patratulMeu.afiseazaPerimetru();
   }
 
 
   @Test
-  public void testDreptunghi () {
+  private void testDreptunghi () {
 
     FiguraGeometrica
         dreptunghiulMeu
         = FiguriGeometriceFactory.creazaFigura(FiguriGeometrice.DREPUNGHI);
     dreptunghiulMeu.calculeazaPerimetru();
+    Assertions.assertEquals(18, dreptunghiulMeu.calculeazaPerimetru());
     dreptunghiulMeu.afiseazaPerimetru();
   }
 
