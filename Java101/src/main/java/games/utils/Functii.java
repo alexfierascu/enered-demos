4ba1e166ffd1279104c4043789271a8794eb9bf7
andreiudar <52848256+andreiudar@users.noreply.github.com>
2019-11-29 19:42:25 +0200

    afisareCuvantModAscuns

---
+++ Java101/src/main/java/games/utils/Functii.java
@@ -0,0 +1,15 @@
+package games.utils;
+
+/**
+ * Created by Andrei on 11/29/2019.
+ */
+public class Functii {
+
+   public static void afisareCuvantModAscuns(String[] cuvant){
+        //cuvant={"a","l","c"};
+        for (int i=0; i<cuvant.length;i++){
+            System.out.print("*");
+        }
+
+    }
+}
