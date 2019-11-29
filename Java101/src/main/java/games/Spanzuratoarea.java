1872b729af4b94194639ae7cd61881053f281cdd
Alex Fierascu <ioan-alexandru.fierascu@externals.adidas.com>
2019-11-29 20:20:50 +0200

    setup 1

--- Java101/src/main/java/games/Spanzuratoarea.java
+++ Java101/src/main/java/games/Spanzuratoarea.java
@@ -1,13 +1,14 @@
 package games;
 
 
-import games.utils.Functii;
+import static games.utils.Functii.*;
+
 
 public class Spanzuratoarea {
 
   public static void main (String[] args) {
 
-   
+    afisareCuvantModAscuns(stringToVector(citireCuvant()));
   }
 
 }
--- Java101/src/main/java/games/utils/Functii.java
+++ Java101/src/main/java/games/utils/Functii.java
@@ -1,4 +1,6 @@
 package games.utils;
+
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -9,33 +11,38 @@ import java.util.stream.Stream;
 
 public class Functii {
 
-    public static String citireCuvant() {
-        String valoareGhicit = null;
-        String path = "E:\\JAVA\\enered-demos\\Java101\\src\\main\\resources\\spanzuratoare.txt";
-        StringBuilder content = new StringBuilder();
-        try (Stream<String> lines = Files.lines(Paths.get(path), StandardCharsets.UTF_8)) {
-            lines.forEach(s -> content.append(s));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        valoareGhicit = content.toString();
-        return valoareGhicit;
+  public static String citireCuvant () {
+
+    String valoareGhicit = null;
+    String path = "C:\\proiecte\\EnerED\\EneredDemos\\Java101\\src\\main\\resources" +
+        "\\spanzuratoare.txt";
+    StringBuilder content = new StringBuilder();
+    try (Stream<String> lines = Files.lines(Paths.get(path), StandardCharsets.UTF_8)) {
+      lines.forEach(s -> content.append(s));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    valoareGhicit = content.toString();
+    return valoareGhicit;
+  }
+
+
+  public static Vector stringToVector (String s) {
+
+    Vector myVec = new Vector();
+    char[] sChars = s.toCharArray();
+    for (int i = 0; i < s.length(); ++i) {
+      myVec.add(sChars[i]);
+    }
+    return (myVec);
+  }
+
+
+  public static void afisareCuvantModAscuns (Vector cuvant) {
+
+    for (int i = 0; i < cuvant.size(); i++) {
+      System.out.print("*");
     }
 
-
-    public static Vector stringToVector(String s) {
-        Vector myVec = new Vector();
-        char[] sChars = s.toCharArray();
-        for (int i = 0; i < s.length(); ++i) {
-            myVec.add(sChars[i]);
-        }
-        return (myVec);
-    }
-
-    public static void afisareCuvantModAscuns(Vector cuvant) {
-        for (int i = 0; i < cuvant.size(); i++) {
-            System.out.print("*");
-        }
-
-    }
+  }
 }
